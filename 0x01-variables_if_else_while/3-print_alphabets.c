a1dccf1c9e9b986a675af53e24961f5b8190cf86
eja abraham <ejaabrahamjr@gmail.com>
2023-06-19 18:42:27 -0700

    ghj

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,17 @@
-nclude <stdio.h>
+include <stdio.h>
 /**
  *  * main - Prints the alphabet.
  *   *
  *    * Return: Always 0 (Success)
- *     */
+*/
 int main(void)
 {
 	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	        int i;
-		        for (i = 0; i < 52; i++)
-				        {
-						                putchar(alp[i]);
-								        }
-			        putchar('\n');
-				        return (0);
+	int i;
+	for (i = 0; i < 52; i++)
+	{
+					                putchar(alp[i]);
+						        }
+	putchar('\n');
+	return (0);
 }

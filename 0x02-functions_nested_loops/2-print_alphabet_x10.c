f2524b5517ab5efd77e65abf40ee63200e779eae
eja abraham <ejaabrahamjr@gmail.com>
2023-06-21 07:58:18 -0700

    jsjs

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
-
-
 	/**
 	 * print ten times the alphabet in lowercase
 	 * followed by a new line 
 	 */
 void print_alphabet_x10(void)
 {
+	/**
+	 * print ten times the alphabet
+	 */
 	char ch;
 	int i;
-
 	i = 0;
-
 	while (i < 10)
 	{
 		ch = 'a';

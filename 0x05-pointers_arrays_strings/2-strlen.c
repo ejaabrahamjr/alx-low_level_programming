554f1f8d38c5b6994635f2953aab18f0a616f63f
eja abraham <ejaabrahamjr@gmail.com>
2023-06-27 05:11:10 -0700

    h

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,15 @@
 /**
  *  * _strlen - returns the length of a string
  *   * @s: string
- *    * Return: length
+ *	    * Return: length
+ *	    inters_arrays_strings
 */
 
 int _strlen(char *s)
 {
+	
 	int longi = 0;
+
 	while (*s != '\0')
 {	
 	longi++;
